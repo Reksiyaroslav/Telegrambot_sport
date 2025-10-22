d679d1dc557bb833f8ac1c74982e8034136f7baa
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-10-22 13:25:15 +0300

    Create handler.py

--- app/handler/handler.py
+++ app/handler/handler.py
@@ -1,2 +1,10 @@
-from aiogram import Router
+from aiogram import Router,F
+from aiogram.types import Message,CallbackQuery
+from aiogram.filters import CommandStart, Command
+import app.keyboard as kb 
+import app.func.func as fun 
+from  app.text import text_hello
 router = Router()
+@router.message(CommandStart())
+async def cmd_start(message:Message):
+    await message.answer(text_hello,reply_markup=kb.keyboard_main)
