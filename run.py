c8b2109a0d493415c86953731d5dd86423107392
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-10-30 02:26:11 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,26 @@
-from aiogram import Bot,Dispatcher
+from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-import os 
+import os
 import asyncio
 from app.handler.handler import router
+
+
 async def main():
     load_dotenv()
-    TOKEn_BOT = os.getenv("TOKEN")
-    bot = Bot(token=TOKEn_BOT)
-    dis =  Dispatcher(bot) 
+    TOKE_BOT = os.getenv("TOKEN")
+    bot = Bot(token=TOKE_BOT)
+    dis = Dispatcher()
     dis.include_router(router)
-    dis.start_polling()
+    await dis.start_polling(bot)
 
-if __name__ =="__run__":
+
+if __name__ == "__main__":
     try:
         print("One bot")
-        asyncio.run(main=main)
+        asyncio.run(main=main())
     except KeyboardInterrupt:
         print("Exit")
     except TimeoutError:
+        print("Off bot")
+    except RuntimeError:
         print("Off bot")
