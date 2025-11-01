7ff789ddccc369a29e98973edecc2d6a0bac3dda
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-11-01 21:19:57 +0300

    update keyboard_inlien

--- app/keyboard/keyboard_inlien.py
+++ app/keyboard/keyboard_inlien.py
@@ -8,29 +8,33 @@ from app.text import (
     text_keyboar_winer,
     text_keyboar_schedule,
     text_message_tourney,
+    text_barselona,
+    text_bavariya,
+    text_relal_madrit,
+    text_clubs
 )
 
 keyboard_main = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text=text_keyboard_main, callback_data="loream")],
-        [
-            InlineKeyboardButton(
-                text=text_keyboard_group_plaers, callback_data="info_group"
-            )
-        ],
+        [InlineKeyboardButton(text=text_clubs,callback_data="clubs")],
     ]
 )
-keyboard_plaer_cohuct = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text=text_keyboard_plaers, callback_data="plaers_data")],
-        [InlineKeyboardButton(text=text_keyboard_coach, callback_data="coach")],
-    ]
-)
+
 keyboard_status = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text=text_keyboar_lose, callback_data="lose")],
         [InlineKeyboardButton(text=text_keyboar_winer, callback_data="winer")],
         [InlineKeyboardButton(text=text_message_tourney, callback_data="tourney")],
         [InlineKeyboardButton(text=text_keyboar_schedule, callback_data="schedule")],
+        [InlineKeyboardButton(text=text_keyboard_plaers, callback_data="plaers_data")],
+        [InlineKeyboardButton(text=text_keyboard_coach, callback_data="coach")],
+    ]
+)
+keyboard_clubs = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text=text_barselona,callback_data="barselona")],
+        [InlineKeyboardButton(text=text_bavariya,callback_data="bavariya")],
+        [InlineKeyboardButton(text=text_relal_madrit,callback_data="real madrit")],
     ]
 )
