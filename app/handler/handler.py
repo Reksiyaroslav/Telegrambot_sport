74d9f7ea36a81bfda9968fb429c3d1aa1f536940
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-11-04 22:03:18 +0300

    update handler.py

--- app/handler/handler.py
+++ app/handler/handler.py
@@ -3,21 +3,21 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from app.keyboard import keyboard_inlien as kb
 import app.func.func as fun
-from app.text import (
-    text_hello,
-    text_group,
-    text_help,
-)
+from app.text import text_hello, text_group, text_help, text_club_varibal
+from app.list import list_club_list, list_key_list
 
 router = Router()
 count = 0
 text = ""
 
+
 class FConte:
-    type_club:str = ""
+    type_club: str = ""
+
+
 # Самое первое сообщение которые видет пользователь выбирает тип
 @router.message(CommandStart())
-async def cmd_start(message: Message):
+async def start_message(message: Message):
     await message.answer(text_hello, reply_markup=kb.keyboard_main)
 
 
@@ -28,36 +28,23 @@ async def help_start(message: Message):
 
 @router.callback_query(F.data == "clubs")
 async def info_clubs(callback: CallbackQuery):
+    await callback.answer(text=text_club_varibal + callback.data)
     await callback.message.answer("Выбери клуб который вам нужен")
     await callback.message.answer(text_group, reply_markup=kb.keyboard_clubs)
-@router.callback_query(F.data == "barselona")
-async def barselona_club(callback: CallbackQuery):
-    FConte.type_club = "Barselona"
-    await callback.message.answer("Теперь что хотите найти")
-    await callback.message.answer(text_group, reply_markup=kb.keyboard_status)
-@router.callback_query(F.data == "bavariya")
+
+
+@router.callback_query(F.data.in_(["bavariya", "real_madrid", "barselona"]))
 async def varabal_club(callback: CallbackQuery):
-    FConte.type_club = "Bavariya"
+    FConte.type_club = callback.data
+    await callback.answer(text=text_club_varibal + FConte.type_club)
     await callback.message.answer("Теперь что хотите найти")
     await callback.message.answer(text_group, reply_markup=kb.keyboard_status)
-@router.callback_query(F.data == "real madrit")
-async def real_mandrit_club(callback: CallbackQuery):
-    FConte.type_club = "Real madrit"
-    await callback.message.answer("Теперь что хотите найти")
-    await callback.message.answer(text_group, reply_markup=kb.keyboard_status)
-@router.callback_query(F.data == "plaers_data")
-async def info_players(callback: CallbackQuery):
-    await callback.message.edit_text(f"Игроки: {FConte.type_club}")
-    await fun.plaers_list(FConte.type_club)
-    text_list: list[str] = await fun.create_message(FConte.type_club, "players")
-    text_full = await fun.full_text(text_list)
-    await callback.message.answer(text=text_full)
-@router.callback_query(F.data == "schedule")
-async def info_schedule(callback: CallbackQuery):
-    await callback.message.edit_text(f"Schedule: {FConte.type_club}")
-    await fun.schedule(FConte.type_club)
-    text_list: list[str] = await fun.create_message(FConte.type_club, "schedule")
-    text_full = await fun.full_text(text_list)
+
+
+@router.callback_query(F.data.in_(["players", "coauth", "schedule"]))
+async def info_type(callback: CallbackQuery):
+    await callback.message.edit_text(f"Coauth: {FConte.type_club}")
+    text_full = await fun.create_message(FConte.type_club, callback.data)
     await callback.message.answer(text=text_full)
 
 
@@ -70,30 +57,28 @@ async def info_group(callback: CallbackQuery):
 @router.callback_query(F.data == "loream")
 async def info_group(callback: CallbackQuery):
     await callback.message.edit_text("Loream")
-    text = await fun.parsing_loream()
+    text = await fun.create_message(callback.data)
     await callback.message.answer(text=text)
 
 
 @router.message(F.text)
-async def message_stop(message:Message):
-    clubs = ("barselona","real madrit","bavariya")
-    if message.text.lower() in clubs:
+async def handler_soup(message: Message):
+    clubs = list_club_list
+    new_message = message.text.lower()
+    if new_message in clubs:
         FConte.type_club = message.text
         await message.answer(f"Вы выбрали <b>{message.text}</b>")
-    elif message.text == "schedule" or message.text =="players" and  FConte.type_club!="":
+    if (
+        new_message == "schedule"
+        or new_message == "players"
+        or new_message == "coauth"
+        and FConte.type_club != ""
+    ):
         message.answer(f"Вы выбрали <b>{message.text}</b>")
-        if message.text == "players":
-            await fun.plaers_list(FConte.type_club)
-        elif message.text == 'schedule':
-            await fun.schedule(FConte.type_club)
-        text_list =await fun.create_message(FConte.type_club,message.text)
-        text_full = await fun.full_text(text_list)
+        text_full = await fun.create_message(FConte.type_club, message.text)
         await message.answer(text=text_full)
-    elif message.text =="loream":
-        text = await fun.parsing_loream
+    if new_message == "loream":
+        text = await fun.create_message(FConte.type_club, message.text)
         await message.answer(text)
     else:
         await message.answer("Нет такой команды")
-
-
-        
