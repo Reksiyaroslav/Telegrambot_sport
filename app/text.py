7c3220200270a5b77d15c80165c702f4b03afa03
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-11-04 23:14:29 +0300

    update text.py

--- app/text.py
+++ app/text.py
@@ -1,13 +1,12 @@
-text_hello = "Привет болешик выберай что будешь искать"
-text_group = """Хотите выбрать расписание мачей  
-или сотав команды или Сколько было побед и поражений итоги их турнира  """
+text_hello = "Привет болельщик! Выбирай ,что будешь искать?"
+text_group = """Хотите выбрать расписание матчей, состав команды, тренер?"""
 text_keyboard_plaers = "Игроки"
-text_keyboard_coach = "Тренира"
+text_keyboard_coach = "Тренер"
 text_keyboar_winer = "Победы "
 text_keyboar_lose = "Поражения"
-text_keyboar_schedule = "Распсиание мачей"
+text_keyboar_schedule = "Расписание матчей"
 text_keyboar_tourney = "Турниры"
-text_keyboar_group = "Встреча с фанатами или болешиками"
+text_keyboar_group = "Встреча с фанатами или болельщиками"
 text_message_group = "Вы выбрали команду"
 text_message_plaers = "Вы выбарли игроков"
 text_message_schedule = "Вы выбрали трениров"
@@ -17,22 +16,24 @@ text_message_tourney = "Вы выбрали турниры и кубки"
 text_message_goul = "Вы выбрали голы и очки"
 text_keyboard_group_plaers = "Данные команды"
 text_keyboard_main = "Loream32"
-text_message_stop = "Тебе нужно выбрать"
-text_help = """Вам нужно нажать на кнопку выбрать команды и и там выбрать что вам важно или вам нужно написать название команды и 
-    потом что хотите сделать Например : real madrit название команды  и потом уже операция players это игроки в клубе  
-    Клубы  которые сделаны:
-    Реал Мадрит : real madrit
-    Барселона : barselona
-    Бавария : bavariya
-    Команды которые есть :
-    players : игроки для клуба
-    schedule : мачи которые буду 
-    coauth : тренер
-    В дальнешем будет комнады :
+text_help = """Вам нужно нажать на кнопку выбрать команды , затем  выбрать то,что вам важно или  написать название команды ,а
+    потом что хотите сделать Например : real_madrid - название команды  ,а  потом  операция players,которая  показывает игроки клуба.  
+    Клубы реализованые :
+    Реал Мадрид: real_madrid
+    Барселона: barselona
+    Бавария: bavariya
+
+    Доступные команды:
+    players - игроки для клуба
+    schedule - предостоящие мачи 
+    coauth - тренер
+    В разработке:
     lose_win: победы и поражения 
     """
 text_barselona = "Барселона"
 text_bavariya = "Бавария"
-text_relal_madrit = "Реал Мадрит"
-text_clubs = "Выбар клана "
-text_club_varibal = "Вы выбрали"
+text_relal_madrit = "Реал Мадрид"
+text_clubs = "Выбор клуба"
+text_club_varibal = "Вы выбрали:"
+text_club_var = "Вы берети клуб"
+text_not_corect = "Нет такой команды"
