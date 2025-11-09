3ed3749789430751f7455313ccf6522b36d47c04
Rexsi <maltzevv.ja@gmail.com>
2025-11-09 19:04:34 +0300

    update handler.py

--- app/handler/handler.py
+++ app/handler/handler.py
@@ -15,28 +15,23 @@ router = Router()
 count = 0
 text = ""
 
-
 class FConte:
     type_club: str = ""
-
 
 # Самое первое сообщение которые видет пользователь выбирает тип
 @router.message(CommandStart())
 async def start_message(message: Message):
     await message.answer(text_hello, reply_markup=kb.keyboard_main)
 
-
 @router.message(Command("help"))
 async def help_start(message: Message):
     await message.answer(text_help)
-
 
 @router.callback_query(F.data == "clubs")
 async def info_clubs(callback: CallbackQuery):
     await callback.answer(text=text_club_varibal + callback.data)
     await callback.message.answer("Выбирай клуб который вам нужен")
     await callback.message.answer(text_club_var, reply_markup=kb.keyboard_clubs)
-
 
 @router.callback_query(F.data.in_(["bavariya", "real_madrid", "barselona"]))
 async def varabal_club(callback: CallbackQuery):
@@ -45,26 +40,22 @@ async def varabal_club(callback: CallbackQuery):
     await callback.message.answer("Теперь что хотите найти")
     await callback.message.answer(text_group, reply_markup=kb.keyboard_status)
 
-
 @router.callback_query(F.data.in_(["players", "coauth", "schedule"]))
 async def info_type(callback: CallbackQuery):
-    await callback.message.edit_text(f"Coauth: {FConte.type_club}")
+    await callback.message.edit_text(f"<b>{callback.data.title()}</b>: <b>{FConte.type_club.title()}</b>")
     text_full = await fun.create_message(FConte.type_club, callback.data)
     await callback.message.answer(text=text_full)
-
 
 @router.callback_query(F.data == "winer")
 async def info_group(callback: CallbackQuery):
     await callback.message.edit_text("Pass")
     await callback.message.answer(text="пока не сделал")
 
-
 @router.callback_query(F.data == "loream")
 async def info_group(callback: CallbackQuery):
     await callback.message.edit_text("Loream")
     text = await fun.create_message(callback.data)
     await callback.message.answer(text=text)
-
 
 @router.message(F.text)
 async def handler_soup(message: Message):
