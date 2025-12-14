767a02ff7c31fcef481e6344fc53d93b6291eecb
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-12-14 10:46:22 +0300

    chages

--- app/config.py
+++ app/config.py
@@ -66,7 +66,7 @@ async def remove_date(data:dict,type_operation):
                 
 async def theer_day_fille(filename: str):
     now_time = time.time()
-    theer_age_day = now_time - 2 * 24 * 60 * 60
+    theer_age_day = now_time - 3 * 24 * 60 * 60
     if os.path.exists(filename):
         time_fille_update = os.path.getmtime(filename=filename)
         return time_fille_update < theer_age_day
